dbab6b1197beb23aa296f50b507f514298c73196
Shadow <prathviksankaliya@gmail.com>
2022-08-26 22:52:04 +0530

    Half Firebase Google Auth Added.

--- app/src/main/java/com/itcraftsolution/allfirebaseauth/NextActivity.java
+++ app/src/main/java/com/itcraftsolution/allfirebaseauth/NextActivity.java
@@ -1,23 +1,42 @@
 package com.itcraftsolution.allfirebaseauth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.itcraftsolution.allfirebaseauth.databinding.ActivityNextBinding;
 
 public class NextActivity extends AppCompatActivity {
 
     private ActivityNextBinding binding;
-
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityNextBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("557577465098-q1pi60mpfv51alvg0l7cv05388tj8foj.apps.googleusercontent.com")
+                        .requestEmail()
+                                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(NextActivity.this, googleSignInOptions);
 
         binding.btnEmail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,10 +48,30 @@ public class NextActivity extends AppCompatActivity {
         binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, 100);
             }
         });
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100)
+        {
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(user != null)
+        {
+            startActivity(new Intent(NextActivity.this, MainActivity.class)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
 }
+
