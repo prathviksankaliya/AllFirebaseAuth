c8ba7f9029f3ebf160268545c3d9ca8589ef5db3
Shadow <prathviksankaliya@gmail.com>
2022-08-28 17:32:57 +0530

    Google Firebase Auth Done.

--- app/src/main/java/com/itcraftsolution/allfirebaseauth/MainActivity.java
+++ app/src/main/java/com/itcraftsolution/allfirebaseauth/MainActivity.java
@@ -1,11 +1,17 @@
 package com.itcraftsolution.allfirebaseauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.itcraftsolution.allfirebaseauth.databinding.ActivityMainBinding;
@@ -15,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private FirebaseAuth auth;
     private FirebaseUser user;
+    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInOptions googleSignInOptions;
 
 
     @Override
@@ -29,14 +37,35 @@ public class MainActivity extends AppCompatActivity {
         if(user != null)
         {
             binding.textView.setText(user.getEmail());
+            binding.textView2.setText(user.getDisplayName());
         }
 
         binding.btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                auth.signOut();
-                startActivity(new Intent(MainActivity.this, registerEmailActivity.class));
-                finish();
+                //Sign in with Email password
+
+//                auth.signOut();
+//                startActivity(new Intent(MainActivity.this, registerEmailActivity.class));
+//                finish();
+
+                //SignIn with Google Auth
+
+                googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+                googleSignInClient = GoogleSignIn.getClient(MainActivity.this, googleSignInOptions);
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            auth.signOut();
+                            startActivity(new Intent(MainActivity.this, NextActivity.class));
+                            finish();
+                        }
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/com/itcraftsolution/allfirebaseauth/NextActivity.java
+++ app/src/main/java/com/itcraftsolution/allfirebaseauth/NextActivity.java
@@ -1,17 +1,32 @@
 package com.itcraftsolution.allfirebaseauth;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.itcraftsolution.allfirebaseauth.databinding.ActivityNextBinding;
 
 public class NextActivity extends AppCompatActivity {
@@ -20,6 +35,7 @@ public class NextActivity extends AppCompatActivity {
     private GoogleSignInClient googleSignInClient;
     private FirebaseUser user;
     private FirebaseAuth auth;
+    private ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +48,7 @@ public class NextActivity extends AppCompatActivity {
 
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(
                 GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("557577465098-q1pi60mpfv51alvg0l7cv05388tj8foj.apps.googleusercontent.com")
+                .requestIdToken("557577465098-0012rsjlg33degee24v155kuqu536rri.apps.googleusercontent.com")
                         .requestEmail()
                                 .build();
 
@@ -49,18 +65,49 @@ public class NextActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = googleSignInClient.getSignInIntent();
-                startActivityForResult(intent, 100);
+                launcher.launch(intent);
             }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100)
-        {
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == Activity.RESULT_OK)
+                {
+                    Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
 
-        }
+                    if(signInAccountTask.isSuccessful())
+                    {
+                        Toast.makeText(NextActivity.this, "Google SignIn is Successful !!", Toast.LENGTH_SHORT).show();
+
+                        try {
+                            GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
+                            if(googleSignInAccount != null)
+                            {
+                                AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+                                auth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful())
+                                        {
+                                            startActivity(new Intent(NextActivity.this, MainActivity.class));
+                                            Toast.makeText(NextActivity.this, "Auth SuccessFul!!", Toast.LENGTH_SHORT).show();
+                                        }else{
+                                            Toast.makeText(NextActivity.this, "Auth Failed!!", Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
+                            }
+                        } catch (ApiException e) {
+                            e.printStackTrace();
+                        }
+                    }else{
+                        Toast.makeText(NextActivity.this, "Failed signIn Account", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -68,8 +115,7 @@ public class NextActivity extends AppCompatActivity {
         super.onStart();
         if(user != null)
         {
-            startActivity(new Intent(NextActivity.this, MainActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            startActivity(new Intent(NextActivity.this, MainActivity.class));
         }
     }
 
